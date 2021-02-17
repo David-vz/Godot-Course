663e67f22b7b34f029a72f1be97fe34eed828a42
David van Zyl <davidvanzyl@gmail.com>
2021-02-17 20:11:14 +0200

    Adds LineEdit
    Learned:
    - How to use LineEdit
    - Signals
    - Calling a func with an argument

--- 3-looney-lips/LooneyLips.gd
+++ 3-looney-lips/LooneyLips.gd
@@ -3,6 +3,11 @@ extends Control
 func _ready():
 	var prompts = ["Dylan", "hates", "tomatoes", "strange", "fried tomato with onions", "delicious"]
 	var story = "My friend %s %s %s, which is %s, since he used to think %s is %s"
-	
-	$DisplayText.text = story % prompts
- 
+	$VBoxContainer/DisplayText.text = story % prompts
+
+func _on_PlayerText_text_entered(new_text):
+	update_DisplayText(new_text)
+
+func update_DisplayText(words):
+	$VBoxContainer/DisplayText.text = words
+	$VBoxContainer/PlayerText.clear();
