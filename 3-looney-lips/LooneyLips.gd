9115169509a60b6e07b1617167d034c264a8c875
David van Zyl <davidvanzyl@gmail.com>
2021-02-17 18:58:09 +0200

    Arrays!
    learn about godot's string replacement function for arrays

--- 3-looney-lips/LooneyLips.gd
+++ 3-looney-lips/LooneyLips.gd
@@ -1,11 +1,7 @@
 extends Control
 
 func _ready():
-	var person = "David"
-	var movie = "Minions"
+	var prompts = ["David", "Minions", "Greatest"]
+	var story = "Once upon a time %s watched %s and thought it was the %s movie of the past two decades"
 	
-	print("Once upon a time " + person +" watched Minions and thought it was the geratest movie of the past two decades)")
-	
-	
-	person = "Someone else"
-	print("Once upon a time " + person +" watched "+movie+ " and thought it was the geratest movie of the past two decades)")
+	print(story % prompts)
