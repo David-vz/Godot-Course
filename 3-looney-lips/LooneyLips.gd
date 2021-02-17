94acbee49c2dc034b265f68034038a50342e15ee
David van Zyl <davidvanzyl@gmail.com>
2021-02-17 19:50:39 +0200

    Dynamically change label text
    Summary of things learned:
    Create background texture
    Import assets in to Godot
    Change a node type
    Resize game's display
    Display text with Label
    Change the properties of a child node

--- 3-looney-lips/LooneyLips.gd
+++ 3-looney-lips/LooneyLips.gd
@@ -4,5 +4,5 @@ func _ready():
 	var prompts = ["Dylan", "hates", "tomatoes", "strange", "fried tomato with onions", "delicious"]
 	var story = "My friend %s %s %s, which is %s, since he used to think %s is %s"
 	
-	print(story % prompts)
+	$DisplayText.text = story % prompts
  
