4787e17d970b34793d54717234d4f2ac6e08db8b
David van Zyl <davidvanzyl@gmail.com>
2021-03-15 19:43:21 +0200

    Adds player character + basic movement
    Adds player character + basic movement

---
+++ 4-hoppy-days/Player/Player.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+var motion = Vector2(0,0)
+
+const SPEED = 1000
+
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("left") and Input.is_action_pressed("right"):
+		motion.x = 0
+	elif Input.is_action_pressed("left"):
+		motion.x = -SPEED
+	elif Input.is_action_pressed("right") :
+		motion.x = SPEED
+	else:
+		motion.x = 0
+	move_and_slide(motion)
